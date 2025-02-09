e745d15befff6b5e91a72965943725787446dc64
Mark Hollomon <mhhollomon@gmail.com>
2025-02-09 11:01:31 -0500

    fix booboo

---
+++ src/feeds/muwee.py
@@ -0,0 +1,18 @@
+from src.feeds.feeddata import FeedData
+from src.db.database import Post
+
+MUWEE_DID : str = 'did:plc:f3mkgbj4ibmtmn2u4b2dpgid'
+
+shortname : str = 'muwee-posts'
+
+def filter(author : str, record : dict) -> bool :
+    lower_text : str = record['text'].lower()
+    return ((not 'reply' in record) and 
+            (('#muwee' in lower_text)
+             or ('@musicweeklies.bsky.social' in lower_text)
+             or author == MUWEE_DID))
+
+def get_posts() :
+    return Post.select().where(Post.feed == shortname)
+
+feed = FeedData(shortname, filter, get_posts)
